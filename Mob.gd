9ed70e850e9e6d89d96ced72bcb5abd20b9b79eb
Victor Rogers <victor.r.rogers@gmail.com>
2024-07-26 15:03:39 -0500

    mob logic implemented

--- Mob.gd
+++ Mob.gd
@@ -1,10 +1,18 @@
 extends CharacterBody3D
 
-# Minimum speed of the mob in meters per second
 @export var min_speed: int = 10
-
-# Maximum speed of the mob in meters per second
 @export var max_speed: int = 18
 
 func _physics_process(_delta: float) -> void:
-    move_and_slide()
+	move_and_slide()
+	
+func initialize(start_position: Vector3, player_position: Vector3) -> void:
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	var random_speed: int = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
