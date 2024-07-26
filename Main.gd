e5e52104210cc9e1510dbf4a3549ebb5455e958b
Victor Rogers <victor.r.rogers@gmail.com>
2024-07-26 16:00:52 -0500

    physics layers

--- Main.gd
+++ Main.gd
@@ -10,3 +10,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob: Node = mob_scene.instantiate()
+	
+	var mob_spawn_location: Node = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	var player_position: Vector3 = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	add_child(mob)
